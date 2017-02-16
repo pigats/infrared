18cecd83eabbdf8ac532de7fe53ab1e6323c3bdf
pigats <andreapigato@gmail.com>
2017-02-16 02:31:30 +0100

    remove useless casting

--- app/controllers/devices/new.js
+++ app/controllers/devices/new.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     actions: {
         create() {
-            this.get('store').findRecord('deviceType', +this.get('typeId')).then((type) => {
+            this.get('store').findRecord('deviceType', this.get('typeId')).then((type) => {
                 this.set('model.device.type', type);
                 this.get('model.device').save();
             });
