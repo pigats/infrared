cd8792d5a251fe0abaffc1f75d9e6629430a4466
pigats <andreapigato@gmail.com>
2017-02-16 10:32:25 +0100

    refactor models to enable select controls

--- app/models/control.js
+++ app/models/control.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    type: DS.belongsTo('controlType')
+    type: DS.belongsTo('controlType'),
+    values: DS.attr({ defaultValue: () => [] })
 });
