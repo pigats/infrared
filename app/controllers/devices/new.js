088fe0685960e93f4ec3d1da751e0e784975600b
pigats <andreapigato@gmail.com>
2017-02-16 11:32:17 +0100

    transition to `devices.settings.edit` after create

--- app/controllers/devices/new.js
+++ app/controllers/devices/new.js
@@ -5,7 +5,9 @@ export default Ember.Controller.extend({
         create() {
             this.get('store').findRecord('deviceType', this.get('typeId')).then((type) => {
                 this.set('model.device.type', type);
-                this.get('model.device').save();
+                this.get('model.device').save().then(() =>
+                    this.transitionToRoute('devices.settings.edit', this.get('model.device'))
+                );
             });
         }
     }
