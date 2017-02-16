6f212781174dc5aedfb44bb25bb23196ec810005
pigats <andreapigato@gmail.com>
2017-02-16 17:57:05 +0100

    add device delete feature

--- app/controllers/devices/settings/edit.js
+++ app/controllers/devices/settings/edit.js
@@ -4,6 +4,13 @@ export default Ember.Controller.extend({
     actions: {
         update() {
             this.get('model.settings').forEach((setting) => setting.save());
+        },
+
+        destroyDevice(device) {
+            if(window.confirm(`Are you sure you want to delete ${device.get('name')}?`)) {
+                device.destroyRecord().then(() => this.transitionToRoute('devices.index'));
+            }
         }
+
     }
 });
